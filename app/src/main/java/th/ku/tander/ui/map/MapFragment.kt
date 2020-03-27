27bd932dcfcdbd54dec9ccaf00f2f7bd2f344e80
Pornpat Santibuppakul <dottypurkt@gmail.com>
2020-03-27 08:15:29 +0700

    Setting map

--- app/src/main/java/th/ku/tander/ui/map/MapFragment.kt
+++ app/src/main/java/th/ku/tander/ui/map/MapFragment.kt
@@ -4,28 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_map.*
 import th.ku.tander.R
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
-    private lateinit var mapViewModel: MapViewModel
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        mapViewModel =
-                ViewModelProviders.of(this).get(MapViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_map, container, false)
-        val textView: TextView = root.findViewById(R.id.text_map)
-        mapViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        return view
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val kaset = LatLng(13.8476, 100.5696)
+        mMap.addMarker(MarkerOptions().position(kaset).title("Here I am"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kaset, 16.0f))
+    }
+
 }
